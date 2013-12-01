8ecb4e45910fe50704bbc50adc834c7401bf23c0
Christian Rasmussen <christian.rasmussen@me.com>
2013-12-01 16:30:55 +0100

    Added more functionality.

--- src/master_project/core.clj
+++ src/master_project/core.clj
@@ -1,21 +1,56 @@
 (ns master-project.core
-  (:require [clojure.java.io :as io]
+  (:require [clojure.core.async :as async :refer :all :rename {map a-map, into a-into, reduce a-reduce, merge a-merge, partition-by a-partition-by, partition a-partition, take a-take}]
+            [clojure.java.io :as io]
             [master-project.tika :as tika]
             [master-project.solr :as solr]
             [master-project.google-translate :as google-translate]
             [master-project.google-search :as google-search]))
 
-(defn get-text-files
-  [path]
-  (let [directory (io/file path)
-        files (file-seq directory)
-        text-file? #(-> % .getName (.endsWith ".txt"))]
-    (filter text-file? files)))
+;; (defn get-text-files
+;;   [path]
+;;   (let [directory (io/file path)
+;;         files (file-seq directory)
+;;         text-file? #(-> % .getName (.endsWith ".txt"))]
+;;     (filter text-file? files)))
 
-(defn translate-file-at-path
-  [path callback]
-  (let [text (extract-text path)]
-    (translate text callback)))
+;; (defn translate-file-at-path
+;;   [path callback]
+;;   (let [text (extract-text path)]
+;;     (translate text callback)))
+
+;; (defn train
+;;   [path]
+;;   ())
+
+;; (defn- search
+;;   [words]
+;;   (let [query (clojure.string/join " " words)]
+;;     (println (str "Searching for: " query))
+;;     (google-search/search query #(println (take 3 %)))))
+
+
+;; (defn fn-with-callback
+;;   [value callback]
+;;   (callback value))
+
+;; (fn-with-callback "test" println)
+
+;; (let
+;;   [ch (chan)]
+;;   (go (fn-with-callback '("a" "b") #(>!! ch %)))
+;;   (go (println (<!! ch))))
+
+(defn get-links
+  [docid]
+  (let [words-ch (chan)
+        links-ch (chan)]
+    (println (str "Finding links for docid: " docid))
+    (go (solr/get-popular-words docid #(>!! words-ch %)))
+    (go (google-search/search (clojure.string/join " " (<!! words-ch)) #(>!! links-ch %)))
+    (go (println (take 3 (<!! links-ch))))))
+
+(get-links "book2")
+;; (get-links "book3")
 
 ;; (println (get-text-files "/Users/skohorn/Sites/Faginnhold/"))
 
--- src/master_project/solr.clj
+++ src/master_project/solr.clj
@@ -2,7 +2,9 @@
   (:require [org.httpkit.client :as http]
             [cheshire.core :as json]))
 
-(declare insert-document-request get-popular-words-request)
+(declare insert-document-request
+         get-popular-words-request
+         clear-database-request)
 
 (defn insert-document
   [id title content callback]
@@ -11,14 +13,23 @@
 ;; (insert-document "ID" "TITLE" "CONTENT" println)
 
 (defn get-popular-words
-  [id callback]
-  (get-popular-words-request id callback #()))
+  [docid callback]
+  (get-popular-words-request docid callback #()))
 
-(get-popular-words "Test2.txt" println)
+;; (get-popular-words "book2" println)
+
+(defn clear-database
+  [callback]
+  (clear-database-request callback #()))
+
+;; (clear-database println)
+
 
 ;; Private
 
-(defn- generate-json-data
+;; Insert document
+
+(defn- generate-insert-document-json-data
   [id title content]
   (let [data [{:id id, :title title, :content content}]]
     (json/generate-string data {:escape-non-ascii false})))
@@ -39,35 +50,28 @@
   [id title content success failure]
   (let [url "http://localhost:8983/solr/update?commit=true"
         options {:headers {"Content-Type" "application/json"}
-                 :body (generate-json-data id title content)}]
+                 :body (generate-insert-document-json-data id title content)}]
     (http/post url options (insert-document-response-handler success failure))))
 
-(defn- construct-get-popular-words-url
-  [id]
-  (let [url-format "http://localhost:8983/solr/tvrh?q=id:%s&tv.tf_idf=true&wt=json"]
-    (format url-format id)))
 
-(defn- parse-word
-  [key value]
-  ({:word key
-    :tf-idf value}))
+;; Get popular words
 
 (defn- parse-get-popular-words-result
   [body]
   (let [words-vector (get-in (json/parse-string body) ["termVectors" 3 3])
-        ;; ["a", ["tf-idf", 0.5], "b", ["tf-idf", 0.25]]
+        ;; => ["a", ["tf-idf", 0.5], "b", ["tf-idf", 0.25]]
 
         words-map (apply hash-map words-vector)
-        ;; {"a" ["tf-idf", 0.5], "b" ["tf-idf", 0.25]}
+        ;; => {"a" ["tf-idf", 0.5], "b" ["tf-idf", 0.25]}
 
         words-map-list (for [[k v] words-map] {:word k, :tf-idf (second v)})
-        ;; ({:word "a" :tf-idf 0.5} {:word "b" :tf-idf 0.25})
+        ;; => ({:word "a" :tf-idf 0.5} {:word "b" :tf-idf 0.25})
 
         sorted-words-map-list (sort-by :tf-idf words-map-list)
-        ;; ({:word "a" :tf-idf 0.25} {:word "b" :tf-idf 0.5})
+        ;; => ({:word "b" :tf-idf 0.25} {:word "a" :tf-idf 0.5})
 
         sorted-words (map :word sorted-words-map-list)]
-        ;; ("a" "b")
+        ;; => ("b" "a")
     (reverse sorted-words)))
 
 (defn- get-popular-words-response-handler
@@ -77,8 +81,42 @@
       (success (parse-get-popular-words-result body))
       (failure error))))
 
+(defn- construct-get-popular-words-url
+  [id]
+  (let [url-format "http://localhost:8983/solr/tvrh?q=id:%s&tv.tf_idf=true&wt=json"]
+    (format url-format id)))
+
 (defn- get-popular-words-request
-  [id success failure]
-  (let [url (construct-get-popular-words-url id)
-        options {}]
+  [docid success failure]
+  (let [url "http://localhost:8983/solr/tvrh"
+        options {:query-params {:q (str "id:" docid)
+                                :tv.tf_idf true
+                                :wt "json"}}]
     (http/get url options (get-popular-words-response-handler success failure))))
+
+
+;; Clear database
+
+(defn- generate-clear-database-json-data
+  []
+  (let [data {:delete {:query "*:*"}}]
+    (json/generate-string data)))
+
+(defn- parse-clear-database-result
+  [body]
+  (let [status (get-in (json/parse-string body) ["responseHeader" "status"])]
+    (zero? status)))
+
+(defn- clear-database-response-handler
+  [success failure]
+  (fn [{:keys [status headers body error]}]
+    (if-not error
+      (success (parse-clear-database-result body))
+      (failure error))))
+
+(defn- clear-database-request
+  [success failure]
+  (let [url "http://localhost:8983/solr/update?commit=true"
+        options {:headers {"Content-Type" "application/json"}
+                 :body (generate-clear-database-json-data)}]
+    (http/post url options (clear-database-response-handler success failure))))
